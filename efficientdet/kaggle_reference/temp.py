894f1ab662b239bd4928979462dbf62c452e930d
YJ0522771 <jyj0522771@gmail.com>
2022-03-29 06:40:30 +0000

    backup

--- efficientdet/kaggle_reference/temp.py
+++ efficientdet/kaggle_reference/temp.py
@@ -1,1 +1,11 @@
+from pycocotools.coco import COCO
 import pandas as pd
+import json
+
+annotation = "../../../detection/dataset/train.json"
+with open(annotation, 'r') as outfile:
+        annotation_json = (json.load(outfile))
+
+print(annotation_json['annotations'][0])
+
+
