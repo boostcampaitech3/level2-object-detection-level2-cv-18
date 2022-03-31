2d1d53966640368cb53b4f7f534d2004dc7fc5cb
kimkihoon0515 <tega1996@naver.com>
2022-03-31 15:24:17 +0000

    yolox added

---
+++ YOLOX/yolox/data/datasets/coco_classes.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+# Copyright (c) Megvii, Inc. and its affiliates.
+
+COCO_CLASSES = (
+    'General trash',
+    'Paper',
+    'Paper pack',
+    'Metal',
+    'Glass',
+    'Plastic',
+    'Styrofoam',
+    'Plastic bag',
+    'Battery',
+    'Clothing'
+)
